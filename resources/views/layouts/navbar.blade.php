0a9748bb67491fc3f7ffe2c73d8230997f4194f9
Ferry Irawan <27ferryirawan@gmail.com>
2023-03-09 23:50:39 +0700

    image change home

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,12 +4,12 @@
             <img src="{{ asset('images/samanko.png') }}" alt="Logo" style="margin-left: 30px; width: 110px; height: 90px;">
         </a>
         <ul class="navbar-nav">
-            <li><a class="{{ request()->is('/') ? 'active' : ''}}" href="/">Home</a></li>
-            <li><a class="{{ request()->is('/coffee') ? 'active' : ''}}" href="/coffee">Coffee Menu</a></li>
-            <li><a class="{{ request()->is('/bakery') ? 'active' : ''}}" href="/bakery">Bakery Menu</a></li>
-            <li><a class="{{ request()->is('/menu') ? 'active' : ''}}" href="/menu">Our Menu</a></li>
-            <li><a class="{{ request()->is('/reservation') ? 'active' : ''}}" href="/reservation">Reservation</a></li>
-            <li><a class="{{ request()->is('/about-us') ? 'active' : ''}}" href="/about-us">About Us</a></li>
+            <li><a class="{{ request()->is('/') ? 'active' : ''}}" href="/">HOME</a></li>
+            <li><a class="{{ request()->is('/coffee') ? 'active' : ''}}" href="/coffee">COFFEE</a></li>
+            <li><a class="{{ request()->is('/bakery') ? 'active' : ''}}" href="/bakery">BAKERY</a></li>
+            <li><a class="{{ request()->is('/menu') ? 'active' : ''}}" href="/menu">OUR MENU</a></li>
+            <li><a class="{{ request()->is('/reservation') ? 'active' : ''}}" href="/reservation">RESERVATION</a></li>
+            <li><a class="{{ request()->is('/about-us') ? 'active' : ''}}" href="/about-us">ABOUT US</a></li>
             @guest
             <li><a class="login-button" href="/login">Login</a></li>
             @endguest
