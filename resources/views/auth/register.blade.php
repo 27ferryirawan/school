1a276f779aba527c81e1b226964b1ee0c3b28982
aisekatsu <ayodance26889@gmail.com>
2023-03-14 19:31:01 +0700

    register page done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,21 +5,22 @@
     </head>
     @include('layouts/navbar')
     <body>
-        <div class="container">
-            <div class="row justify-content-center" style="margin-top:3rem;">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('Register') }}</div>
-
-                        <div class="card-body">
+    <div class="container" style="padding-bottom:2.4%">
+            <div class="row justify-content-center" style="margin-top:2rem;">
+                <div class="col-md-8 d-flex justify-content-center">
+                    <div class="register">
+                        <div class="register-header">
+                            Register
+                        </div>
+                        <div class="register-body">
                             <form method="POST" action="{{ route('register') }}">
                                 @csrf
 
-                                <div class="row mb-3">
-                                    <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                                <div class="col mb-2 w-50 mx-auto">
+                                    <label for="name" class="row-md-4 row-form-label text-md-end">{{ __('Name') }}</label>
 
-                                    <div class="col-md-6">
-                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                    <div class="row-md-3">
+                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus style="border-radius:30px; background-color: #F0F0F0;">
 
                                         @error('name')
                                             <span class="invalid-feedback" role="alert">
@@ -29,11 +30,25 @@
                                     </div>
                                 </div>
 
-                                <div class="row mb-3">
-                                    <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                <div class="col mb-2 w-50 mx-auto">
+                                    <label for="username" class="row-md-4 row-form-label text-md-end">{{ __('Username') }}</label>
 
-                                    <div class="col-md-6">
-                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                    <div class="row-md-3">
+                                        <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus style="border-radius:30px; background-color: #F0F0F0;">
+
+                                        @error('username')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="col mb-2 w-50 mx-auto">
+                                    <label for="email" class="row-md-4 row-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                                    <div class="row-md-3">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" style="border-radius:30px; background-color: #F0F0F0;">
 
                                         @error('email')
                                             <span class="invalid-feedback" role="alert">
@@ -43,11 +58,11 @@
                                     </div>
                                 </div>
 
-                                <div class="row mb-3">
-                                    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                <div class="col mb-2 w-50 mx-auto">
+                                    <label for="password" class="row-md-4 row-form-label text-md-end">{{ __('Password') }}</label>
 
-                                    <div class="col-md-6">
-                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                    <div class="row-md-3 mb-3">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" style="border-radius:30px; background-color: #F0F0F0;">
 
                                         @error('password')
                                             <span class="invalid-feedback" role="alert">
@@ -57,26 +72,29 @@
                                     </div>
                                 </div>
 
-                                <div class="row mb-3">
-                                    <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                                    <div class="col-md-6">
-                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <div class="row mb-2">
+                                    <div class="col-md-8 offset-md-2 text-center">
+                                        <button type="submit" class="btn button-login">
+                                            <b style="letter-spacing:2px; margin: 0 20px 0 20px; font-size: 20px;">
+                                                Sign Up
+                                            </b>
+                                        </button>
                                     </div>
                                 </div>
 
                                 <div class="row mb-0">
-                                    <div class="col-md-6 offset-md-4">
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Register') }}
-                                        </button>
+                                    <div class="col-md-8 offset-md-2 pb-3 text-center">
+                                        <span style = "color: #ba9e7f">Already Have an Account ?</span>
+                                        <a href="/login" style="color: #D4B096; text-decoration:underline; cursor: pointer;">Login</a>
                                     </div>
                                 </div>
+
                             </form>
                         </div>
-                    </div>
+                    </div>                    
                 </div>
             </div>
         </div>
+        @include('layouts/footer2')
     </body>
 </html>
--- resources/views/layouts/footer2.blade.php
+++ resources/views/layouts/footer2.blade.php
@@ -1,15 +1,15 @@
 <a onclick="topFunction()" id="myBtn" title="Go to top">
     <img src="{{ asset('images/up-arrow.png') }}" alt="Logo" style="width: 30px; height:30px; margin: 0px -24px 50px 0px;">
 </a>
-<div style="display:block; width: 100%">
+<div style="display:block; position:fixed; bottom:0; width: 100%;">
     <div style="position: relative; color: white; font-size: 16px;">
-        <img src="{{ asset('images/samanko_footer.jpg') }}" style="width: 100%; height: 200px; object-fit: cover;">
-        <div style="position: absolute; top: calc(50% - 24px); width: 90%; margin: 0px 50px 0px 50px;">
-            <label>"Every specially coffee drink you have here at Samanko Coffee Roaster is made special from</label>
+        <img src="{{ asset('images/samanko_footer.jpg') }}" style="width: 100%; height: 130px; object-fit: cover;">
+        <div style="position: absolute; top: 20px; width: 90%; margin: 0px 50px 0px 50px;">
+            <label style="font-size:13px">"Every specially coffee drink you have here at Samanko Coffee Roaster is made special from</label>
             <br>
-            <label>un-roasted green beans sourced ethically from the farmer from all over the world to roasted fresh onsite on our roaster.</label>
+            <label style="font-size:13px">un-roasted green beans sourced ethically from the farmer from all over the world to roasted fresh onsite on our roaster.</label>
             <br>
-            <label>This ensures freshness, quality control, and allows us to roast to taste."</label>
+            <label style="font-size:13px">This ensures freshness, quality control, and allows us to roast to taste."</label>
         </div>        
     </div>
 </div>
